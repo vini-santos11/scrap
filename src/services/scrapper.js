128c06d610db0bf1fb142e035083f45e43f2985d
Marcus Vinícius <mv.santos11@hotmail.com>
2024-02-26 21:26:37 -0300

    fix: ajuste numberofreviews

--- src/services/scrapper.js
+++ src/services/scrapper.js
@@ -10,7 +10,7 @@ function scrapper(data) {
         const price = cheerioScrapper(element).find('.a-offscreen').text().trim();
         const rating = cheerioScrapper(element).find('.a-icon-star-small .a-icon-alt').text().trim();
         const imageUrl = cheerioScrapper(element).find('img').attr('src');
-        const numberOfReviews = cheerioScrapper(element).find('.a-size-small .a-link-normal').text().trim();
+        const numberOfReviews = cheerioScrapper(element).find('.a-link-normal .s-underline-text').text().trim();
   
         products.push({ name, price, rating, imageUrl, numberOfReviews });
       });
